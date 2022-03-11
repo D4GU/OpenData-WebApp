92839b795e06b2954fd66eca911dbe9df71026f1
D4GU <daniel.guggisberg@hotmail.ch>
2022-03-11 23:26:22 +0100

    -Added SideNav and TopNav and really annoying Icon

--- modules/hideElement.js
+++ modules/hideElement.js
@@ -1,12 +1,12 @@
 (function ($) {
     $.fn.invisible = function () {
         return this.each(function () {
-            $(this).css("transform", "translateY(-85%)");
+            $(this).css("transform", "translateX(-90%)");
         });
     };
     $.fn.visible = function () {
         return this.each(function () {
-            $(this).css("transform", "translateY(0%)");
+            $(this).css("transform", "translateX(0%)");
         });
     };
 }(jQuery));
@@ -15,12 +15,12 @@ $("#nav1").invisible();
 
 (function () {
     $(document).mousemove(function (e) {
-        mY = e.pageY;
-        if (e.pageY < 125) {
+        mY = e.pageX;
+        if (e.pageX < 10) {
             $("#nav1").visible();
             isNavBarHidden = false;
         }
-        else if (e.pageY >= 125) {
+        else if (e.pageX >= 105) {
             $("#nav1").invisible();
             isNavBarHidden = true;
         }

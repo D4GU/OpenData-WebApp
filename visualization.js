073d6fed2f8b9b3866d47a6c279439ebd734199f
D4GU <daniel.guggisberg@hotmail.ch>
2022-05-11 21:22:42 +0200

    Added switch when changing view

--- visualization.js
+++ visualization.js
@@ -1,6 +1,6 @@
 
-$(document).ready(function() {
-  $('.dropdown-item').click(function(e) {
+$(document).ready(function () {
+  $('.dropdown-item').click(function (e) {
     e.stopPropagation();
     $(this).siblings('.dropdown-item').removeClass('active')
     $(this).toggleClass('active');
@@ -10,19 +10,19 @@ $(document).ready(function() {
 
 var mapContainer = d3.select('#map')
   .style("opacity", 0)
-  .style("overflow-x","hidden")
+  .style("overflow-x", "hidden")
 
 var statisticsContainer = d3.select('#statistics')
-  .style("overflow-x","hidden")
+  .style("overflow-x", "hidden")
 
 var statsvg = statisticsContainer.append('svg')
   .attr("id", "statisticssvg")
-  .attr("width", 100 +"%")
+  .attr("width", 100 + "%")
   .attr("height", 100 + "%")
 
 var mapsvg = mapContainer.append('svg')
   .attr("id", "mapsvg")
-  .attr("width", 100 +"%")
+  .attr("width", 100 + "%")
   .attr("height", 100 + "%")
   .attr("viewBox", [55, 360, 1080, 1])
 
@@ -59,61 +59,61 @@ const zoom = d3.zoom()
   .on("zoom", zoomed);
 
 function main() {
-  createBar() 
+  createBar()
   populateMap()
   mapsvg.call(zoom);
   transitionMap();
   setTimeout(function () {
-    updateValues("Leistung_kw", 2011, 0)
+    updateValues("Leistung_kw", 2011, 0, "dataset1")
   }, 400);
-  
+
 }
 
 function populateMap() {
   d3.json("./node_modules/swiss-maps/2021-07/ch-combined.json").then(function (ch) {
     d3.json("./lib/preprocessing/countrycombined.json").then(function (data) {
-    window.dataset1 = data
-    country.selectAll("path")
-      .data(topojson.feature(ch, ch.objects.country).features)
-      .enter().append("path")
-      .attr("class", "country")
-      .attr("abbreviation", data.abbreviation[0])
-      .attr("name", data.name[0])
-      .attr("data", JSON.stringify(data))
-      .attr("d", path)
-      .on("mouseover", handleMouseOver)
-      .on("mouseout", handleMouseOut)
-      .on("click", handleClick)
+      window.dataset1 = data
+      country.selectAll("path")
+        .data(topojson.feature(ch, ch.objects.country).features)
+        .enter().append("path")
+        .attr("class", "country")
+        .attr("abbreviation", data.abbreviation[0])
+        .attr("name", data.name[0])
+        .attr("data", JSON.stringify(data))
+        .attr("d", path)
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut)
+        .on("click", handleClick)
 
-    });  
-    
-    
+    });
+
+
     d3.json("./lib/preprocessing/cantonscombined.json").then(function (data) {
-        var x = 0;
-        window.dataset2 = data
-        canton.selectAll("path")
-          .data(topojson.feature(ch, ch.objects.cantons).features)
-          .enter().append("path")
-          .each(function (d) {
+      var x = 0;
+      window.dataset2 = data
+      canton.selectAll("path")
+        .data(topojson.feature(ch, ch.objects.cantons).features)
+        .enter().append("path")
+        .each(function (d) {
 
-            d3.select(this)
-              .attr("abbreviation", data.abbreviation[x])
-              .attr("name", data.name[x])
-              .attr("id", x)
-            x++
-          })
-          
-          .attr("class", "canton-boundaries")
-          .attr("d", path)
-          .on("mouseover", handleMouseOver)
-          .on("mouseout", handleMouseOut)
-          .on("click", handleClick)
-          
-  
-      });
+          d3.select(this)
+            .attr("abbreviation", data.abbreviation[x])
+            .attr("name", data.name[x])
+            .attr("id", x)
+          x++
+        })
 
-      d3.csv("./node_modules/swiss-maps/2021-07/municipalitiesV3.csv").then(function (name) {
-        d3.json("./lib/preprocessing/municipalities.json").then(function (data) {
+        .attr("class", "canton-boundaries")
+        .attr("d", path)
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut)
+        .on("click", handleClick)
+
+
+    });
+
+    d3.csv("./node_modules/swiss-maps/2021-07/municipalitiesV3.csv").then(function (name) {
+      d3.json("./lib/preprocessing/municipalities.json").then(function (data) {
         var y = 0;
         window.dataset3 = data
         municipalities.selectAll("path")
@@ -130,8 +130,8 @@ function populateMap() {
           .on("mouseout", handleMouseOut)
           .on("click", handleClick)
       })
-      });
     });
+  });
 
 
 }
@@ -152,8 +152,8 @@ function createBar() {
     .data(colors)
     .enter()
     .append('stop')
-    .style('stop-color', function(d){ return d; })
-    .attr('offset', function(d,i){
+    .style('stop-color', function (d) { return d; })
+    .attr('offset', function (d, i) {
       return 100 * (i / (colors.length - 1)) + '%';
     })
 
@@ -169,7 +169,7 @@ function createBar() {
     .attr('x', '0.5%')
     .attr('y', '-32px')
     .text('Placeholder')
-  
+
   colorScaleBar.append('text')
     .attr('id', 'function')
     .attr('x', '66%')
@@ -186,22 +186,20 @@ function updateColorBar(min, max, title, fnc) {
 
   d3.select('g#colorScaleBar').select('text#function')
     .text(fnc)
-  
+
   let x = d3.scaleLinear()
     .domain([min, max])
     .range([0, 900])
-  
-  
-  console.log()
+
   d3.select('g#g-runoff').remove()
-    
+
 
   let axis = d3.axisBottom(x);
   d3.select('g#colorScaleBar')
     .attr("class", "axis")
     .attr("width", 160)
     .attr("height", 40)
-  .append("g")
+    .append("g")
     .attr("id", "g-runoff")
     .call(axis);
 
@@ -212,19 +210,19 @@ function getTitles() {
   var title;
   var fncname;
 
-  switch(staticattribute) {
+  switch (staticattribute) {
     case 'Leistung_kw':
-      title = "Registered power (kW) in the year "+ staticyear
+      title = "Registered power (kW) in the year " + staticyear
       break;
     case 'Verguetung_chf':
-      title = "Feed-in remunaration (CHF) in the year "+ staticyear
+      title = "Feed-in remunaration (CHF) in the year " + staticyear
       break;
     case 'Produktion_kwh':
-      title = "Registered production (kWh) in the year "+ staticyear
+      title = "Registered production (kWh) in the year " + staticyear
       break;
   }
 
-  switch(staticfnc) {
+  switch (staticfnc) {
     case 0:
       fncname = "Function: Maximum"
       break;
@@ -247,15 +245,16 @@ function getTitles() {
 
   return [title, fncname]
 }
-  
+
 
 
 var staticattribute = "";
 var staticyear = "";
 var staticfnc = null;
+var staticdataset = '';
 
-function updateValues(attribute, year, fnc) {
-  
+function updateValues(attribute, year, fnc, dataset) {
+
   if (attribute != '') {
     staticattribute = attribute;
   }
@@ -265,37 +264,64 @@ function updateValues(attribute, year, fnc) {
   if (fnc != null) {
     staticfnc = fnc;
   }
+  if (dataset != '') {
+    staticdataset = dataset
+  }
 
-  console.log(staticattribute)
-  
-
-  
-  
   let identifier = staticattribute + staticyear
 
   let selection = d3.selectAll('g#country').selectAll("*")
   let selection2 = d3.selectAll('g#canton').selectAll("*")
   let selection3 = d3.selectAll('g#municipality').selectAll("*")
-  
-  var localmax = 0;
-  var localmin = window.dataset2[identifier][0][staticfnc];
 
-  
+  console.log(identifier + " " + staticfnc + " " + staticdataset)
+  // unnecessary redundancy :P
 
-  for (let x = 0; x<Object.keys(window.dataset2[identifier]).length; x++) {
-    if (localmax < window.dataset2[identifier][x][staticfnc]) {
-      localmax = window.dataset2[identifier][x][staticfnc]
+
+  var localmax2 = 0;
+  var localmin2 = window.dataset2[identifier][0][staticfnc];
+
+  for (let x = 0; x < Object.keys(window.dataset2[identifier]).length; x++) {
+    if (localmax2 < window.dataset2[identifier][x][staticfnc]) {
+      localmax2 = window.dataset2[identifier][x][staticfnc]
     }
-    if (localmin > window.dataset2[identifier][x][staticfnc]) {
-      localmin = window.dataset2[identifier][x][staticfnc]
+    if (localmin2 > window.dataset2[identifier][x][staticfnc]) {
+      localmin2 = window.dataset2[identifier][x][staticfnc]
     }
   }
 
-  console.log(getTitles())
-  updateColorBar(localmin, localmax, getTitles()[0], getTitles()[1])
 
-  var colorScale1= getColorscale(0, window.dataset1[identifier][0][staticfnc])
-  var colorScale2 = getColorscale(localmin, localmax)
+  var localmax3 = 0;
+  var localmin3 = 100000000000000;
+
+  x = 0;
+  for (entry in window.dataset3[identifier]) {
+    try {
+      if (localmax3 < window.dataset3[identifier][entry][staticfnc]) {
+        localmax3 = window.dataset3[identifier][entry][staticfnc]
+      }
+      if (localmin3 > window.dataset3[identifier][entry][staticfnc]) {
+        localmin3 = window.dataset3[identifier][entry][staticfnc]
+      }
+    } catch (error) {
+      console.log("Ignored:", error)
+    }
+
+  }
+  // unnecessary redundancy :P
+
+  if (staticdataset == 'dataset1') {
+    updateColorBar(0, window.dataset1[identifier][0][staticfnc], getTitles()[0], getTitles()[1])
+    console.log("here")
+  } if (staticdataset == 'dataset2') {
+    updateColorBar(localmin2, localmax2, getTitles()[0], getTitles()[1])
+  } if (staticdataset == 'dataset3') {
+    updateColorBar(localmin3, localmax3, getTitles()[0], getTitles()[1])
+  }
+
+  var colorScale1 = getColorscale(0, window.dataset1[identifier][0][staticfnc])
+  var colorScale2 = getColorscale(localmin2, localmax2)
+  var colorScale3 = getColorscale(localmin3, localmax3)
 
   selection
     .attr("currentObs", window.dataset1[identifier][0][staticfnc])
@@ -314,31 +340,33 @@ function updateValues(attribute, year, fnc) {
         })
     })
 
-    // selection3
-    // .each(function () {
-    //   selectionName = d3.select(this).attr("nameclean")
-      
-    //   d3.select(this)
-    //     .attr("currentObs", function(d) {
-    //       if (selectionName in window.dataset3[identifier]){
-    //         try {return window.dataset3[identifier][selectionName][staticfnc]
-    //         } catch (error) { 
-    //           return "0"
-    //         }
-    //       } else {
-    //         return "0"
-    //       }
-    //   })
-    //     .style("fill", function (d) {
-    //       if (selectionName in window.dataset3[identifier]){
-    //         return colorScale2(window.dataset3[identifier][selectionName][staticfnc])
-    //       } else {
-    //         return colorScale2(0)
-    //       }
-    //     })
-    // })
+  selection3
+    .each(function () {
+      selectionName = d3.select(this).attr("nameclean")
 
-  
+      d3.select(this)
+        .attr("currentObs", function (d) {
+          if (selectionName in window.dataset3[identifier]) {
+            try {
+              return window.dataset3[identifier][selectionName][staticfnc]
+            } catch (error) {
+              console.log("Ignored:", error)
+              return "0"
+            }
+          } else {
+            return "0"
+          }
+        })
+        .style("fill", function (d) {
+          if (selectionName in window.dataset3[identifier]) {
+            return colorScale2(window.dataset3[identifier][selectionName][staticfnc])
+          } else {
+            return colorScale2(0)
+          }
+        })
+    })
+
+
 }
 
 
@@ -380,7 +408,7 @@ function handleMouseOut(d, i) {
   d3.select(this)
     .style("opacity", 1)
 
-        
+
   tooltip.transition()
     .duration('100')
     .style("opacity", 0);
@@ -388,7 +416,7 @@ function handleMouseOut(d, i) {
 
 function getColorscale(min, max) {
   let colorScale = d3.scaleThreshold()
-    .domain([min, max*0.2, max*0.4, max*0.6, max*0.8, max])
+    .domain([min, max * 0.2, max * 0.4, max * 0.6, max * 0.8, max])
     .range(d3.schemeBlues[5])
   return colorScale
 }
@@ -396,20 +424,20 @@ function getColorscale(min, max) {
 function handleClick(d, i) {
   centroid = getBoundingBoxCenter(d3.select(this))
   d3.selectAll("path")
-    .style("stroke-width",".3")
+    .style("stroke-width", ".3")
     .transition()
     .duration(400)
-      .attr("transform", "scale(1,1)")
+    .attr("transform", "scale(1,1)")
   d3.select(this)
-    .style("stroke-width","2.5")
+    .style("stroke-width", "2.5")
     .raise()
     .transition()
-      .duration(400)
-        .attr("transform", "scale(1.002,1.002)")
+    .duration(400)
+    .attr("transform", "scale(1.002,1.002)")
   d.stopPropagation();
   mapsvg.transition().duration(750).call(
     zoom.transform,
-    d3.zoomIdentity.translate(width/2, height/2).scale(2).translate(-centroid[0]+120, -centroid[1]+85),
+    d3.zoomIdentity.translate(width / 2, height / 2).scale(2).translate(-centroid[0] + 120, -centroid[1] + 85),
     d3.pointer(d)
   )
 }
@@ -430,7 +458,7 @@ function visibilitytoggler(d) {
   }
 }
 
-function zoomed({transform}) {
+function zoomed({ transform }) {
   country.attr("transform", transform);
   canton.attr("transform", transform);
   municipalities.attr("transform", transform);
@@ -444,10 +472,10 @@ function reset() {
   );
 }
 
-function getBoundingBoxCenter (selection) {
+function getBoundingBoxCenter(selection) {
   var element = selection.node();
   var bbox = element.getBBox();
-  return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
+  return [bbox.x + bbox.width / 2, bbox.y + bbox.height / 2];
 }
 
 main();
